a5d98eadc499fb1ae247a709b3048f1b775717c0
lheido <lheido.web@gmail.com>
2015-01-11 15:53:21 +0100

    ajout des fichiers de base pour le projet

---
+++ GruntFile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'none',
+                    update: true
+                },
+                files: {
+                    "assets/css/design.css": "assets/scss/design.scss"
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['css/*.scss', "**/*.html", "**/*.js"],
+                tasks: ['sass:dist']
+            },
+            options: {
+                livereload: true
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-sass')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+
+    grunt.registerTask('default', ['sass:dist'])
+}
