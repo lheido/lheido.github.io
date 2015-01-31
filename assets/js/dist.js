263e43681152121b337243c0b58a358d5e1b6847
lheido <lheido.web@gmail.com>
2015-01-31 12:29:30 +0100

    maj project style, translateX content when appear

--- assets/js/dist.js
+++ assets/js/dist.js
@@ -13,7 +13,9 @@
 var prefixedTransform = Modernizr.prefixed('transform'),
     currentPage = "#home",
     androidProjectsPage = document.querySelector("#android-projects"),
+    androidProjectsPageContent = document.querySelector("#android-projects .content"),
     webProjectsPage = document.querySelector("#web-projects"),
+    webProjectsPageContent = document.querySelector("#web-projects .content"),
     toggleNav = document.querySelector("#toggle-nav"),
     menu = document.querySelector("header"),
     burger = document.querySelector("#burger-wrapper"),
@@ -57,24 +59,30 @@ function selectPage(page) {
     if (!isSmallScreen()) {
         if (page == "#home") {
             translateX(androidProjectsPage, "80%");
+            translateX(androidProjectsPageContent, "50%");
             translateX(webProjectsPage, "90%");
+            translateX(webProjectsPageContent, "50%");
         } else if (page == "#android-projects") {
             translateX(androidProjectsPage, "10%");
+            translateX(androidProjectsPageContent, "0%");
             translateX(webProjectsPage, "90%");
+            translateX(webProjectsPageContent, "50%");
         } else if (page == "#web-projects") {
             translateX(androidProjectsPage, "80%");
+            translateX(androidProjectsPageContent, "50%");
             translateX(webProjectsPage, "10%");
+            translateX(webProjectsPageContent, "0%");
         }
     } else {
         document.querySelector(page).scrollIntoView();
     }
+    currentPage = page; // save current selected page.
 }
 
 //add onclick event on each menu items
 for (var i = 0; i < navItems.length; i++) {
     navItems[i].addEventListener("click", function(event) {
         selectPage(this.hash);
-        currentPage = this.hash; // save current selected page.
         for (var j = 0; j < navItems.length; j++)
             navItems[j].classList.remove("ripple");
         this.classList.add("ripple");
@@ -141,7 +149,9 @@ window.onresize = function() {
                 translateX(menu, "-"+menu.clientWidth+"px");
             }
             translateX(androidProjectsPage, "0%");
+            translateX(androidProjectsPageContent, "0%");
             translateX(webProjectsPage, "0%");
+            translateX(webProjectsPageContent, "0%");
             document.querySelector(currentPage).scrollIntoView();
         } else {
             translateX(menu, "0px");
